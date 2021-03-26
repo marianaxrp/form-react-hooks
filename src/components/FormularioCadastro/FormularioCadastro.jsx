734b0bb13ca4c40bdea28662e81235d020102bd9
mariana <marianaxrp@gmail.com>
2021-03-26 01:19:13 -0300

    added style to the form components using material-ui

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,26 +1,46 @@
-import React from 'react';
-import Button from "@material-ui/core/Button"
+import React from "react";
+import { TextField, Button, Switch, FormControlLabel } from "@material-ui/core";
 
 function FormularioCadastro() {
-    return(
-        <form>
-            <label>Nome</label>
-            <input type="text" />
+  return (
+    <form>
+      <TextField
+        id="nome"
+        label="Nome"
+        variant="outlined"
+        margin="normal"
+        fullWidth
+      />
+      <TextField
+        id="sobrenome"
+        label="Sobrenome"
+        variant="outlined"
+        margin="normal"
+        fullWidth
+      />
+      <TextField
+        id="cpf"
+        label="CPF"
+        variant="outlined"
+        margin="normal"
+        fullWidth
+      />
 
-            <label>Sobrenome</label>
-            <input type="text" />
+      <FormControlLabel
+        label="Promoções"
+        control={<Switch name="promocoes" defaultChecked color="primary" />}
+      />
 
-            <label>CPF</label>
-            <input type="text" />
+      <FormControlLabel
+        label="Novidades"
+        control={<Switch name="novidades" defaultChecked color="primary" />}
+      />
 
-            <label>Promoções</label>
-            <input type="checkbox" />
-            <label>Novidades</label>
-            <input type="checkbox" />
-
-            <Button type="submit" variant="contained" color="primary">Cadastrar</Button>
-        </form>
-    );
+      <Button type="submit" variant="contained" color="primary">
+        Cadastrar
+      </Button>
+    </form>
+  );
 }
 
 export default FormularioCadastro;
